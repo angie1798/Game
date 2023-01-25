02d2474e995fa8de852dad165265d34a654e23e9
Angie López <angie179836@gmail.com>
2023-01-25 12:38:16 -0600

    added while to repeat as long as the guessed num is different, ifadded to know if number is bigger

--- GameProject/Game.cs
+++ GameProject/Game.cs
@@ -11,16 +11,30 @@ namespace GameProject
         static void Main(string[] args)
         {
             RunGame();
+            Console.ReadKey();
         }
         static void RunGame()
         {
             Random rnd = new Random();
-            int randomNumber = rnd.Next(0, 10000);
+            int randomNumber = rnd.Next(0, 100);
             int NumTries = 0;
             int NumGuessed = 0;
 
+            do
+            {
+                Console.WriteLine("Guess the secret number:");
+                NumGuessed = int.Parse(Console.ReadLine());
 
+                //validation to see if the number is bigger
+                if (NumGuessed > randomNumber)
+                {
+                    Console.WriteLine("Too large");
+                }
 
+                NumTries++;
+
+            } while (NumGuessed != randomNumber);
+            
         }
 
     }
