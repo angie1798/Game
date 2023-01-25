fa92d816cb4ff6a852558a592e63d7daec0330bd
Angie López <angie179836@gmail.com>
2023-01-25 12:25:59 -0600

    variables created

--- GameProject/Game.cs
+++ GameProject/Game.cs
@@ -14,6 +14,12 @@ namespace GameProject
         }
         static void RunGame()
         {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 10000);
+            int NumTries = 0;
+            int NumGuessed = 0;
+
+
 
         }
 
